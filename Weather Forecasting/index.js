ca787100819ec4c514dd842bb6128f35f79b52fe
Saketraj <saketbarnwal468@gmail.com>
2025-06-03 16:53:14 +0530

    js changes

--- Weather Forecasting/index.js
+++ Weather Forecasting/index.js
@@ -13,14 +13,11 @@ let w_pressure = document.querySelector(".weather_pressure");
 
 let citySearch = document.querySelector(".weather_search");
 
-
 // to get the actual country name
 
 const getCountryName = (code) => {
-  return new Intl.DisplayNames([code] , { type: "region" }).of(code);
-}
-
-
+  return new Intl.DisplayNames([code], { type: "region" }).of(code);
+};
 
 // to get the date and time
 
@@ -36,57 +33,52 @@ const getDateTime = (dt) => {
     hour: "numeric",
     minutes: "numeric",
   };
-  const formatter = new Intl.DateTimeFormat("en-US" , options);
+  const formatter = new Intl.DateTimeFormat("en-US", options);
   console.log(formatter);
   return formatter.format(curDate);
 };
-let city = "ambala" ;
+let city = "ambala";
 // search functionality
 
-citySearch.addEventListener("submit" , (e) =>{
- e.preventDefault();
- let cityName = document.querySelector(".city_name");
- console.log(cityName.value);
- city = cityName.value;
- getWeatherData();
- cityName.value= "";
-})
-
-
-
+citySearch.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let cityName = document.querySelector(".city_name");
+  console.log(cityName.value);
+  city = cityName.value;
+  getWeatherData();
+  cityName.value = "";
+});
 
 //define the getWeather function here
 
 const getWeatherData = async () => {
   const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=92e739023f5ecf12daee937a949a58ac`;
-  // const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=pune&APPID=a0b06669a793506ae54060f7df8a5fb6`;
+
   try {
     const res = await fetch(weatherUrl);
     const data = await res.json();
     console.log(data);
-    const {main , name , weather , wind , sys ,dt } = data;
+    const { main, name, weather, wind, sys, dt } = data;
     cityName.innerHTML = `${name} ,${getCountryName(sys.country)}`;
     dateTime.innerHTML = getDateTime(dt);
 
     w_forecast.innerHTML = weather[0].main;
     w_icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather[0].icon}@4x.png"/>`;
-    // http://openweathermap.org/img/wn/${weather[0].icon}@4x.png
 
-    let temperature =main.temp;
-    temperature = ((temperature - 273).toFixed(0));
-    w_temperature.innerHTML=  `${(main.temp - 273).toFixed(0)}&#176`;
-   
+    let temperature = main.temp;
+    temperature = (temperature - 273).toFixed(0);
+    w_temperature.innerHTML = `${(main.temp - 273).toFixed(0)}&#176`;
+
     w_minTem.innerHTML = `Min: ${(main.temp_min - 273).toFixed(1)}&#176`;
     w_maxTem.innerHTML = `Max: ${(main.temp_max - 273).toFixed(1)}&#176`;
-    
+
     w_feelsLike.innerHTML = `${(main.feels_like - 273).toFixed(2)}&#176`;
     w_humidity.innerHTML = ` ${main.humidity}%`;
     w_wind.innerHTML = `${wind.speed}m/s`;
     w_pressure.innerHTML = `${main.pressure} hPa`;
-  } catch (error) { 
+  } catch (error) {
     console.log(error);
   }
-
 };
 
 document.body.addEventListener("load", getWeatherData());
